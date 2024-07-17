a16547bbc12cec7a224af35853ec934772db9a42
ZEK <ZEK@DESKTOP-MQI8H1N>
2024-07-17 09:57:21 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Lütfen bir sayı giriniz : ");
+
+int say = Convert.ToInt32(Console.ReadLine());
+
+if (say > 10)
+{
+    Console.WriteLine("sayı 10 dan büyük");
+     if (say % 2 ==0)
+     {
+        Console.WriteLine("sayı çift");
+
+     }
+
+
+
+}
+else
+{
+
+    Console.WriteLine("sayı 10 dan büyük değil ve tek "); // burda else kullanmamım sebebi kullanıcının hangi sayıyı gireceğini bilemememiz
+}
+
+Console.ReadLine();
